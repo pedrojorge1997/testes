67ad49cbe16beb3a360596e7d229826604e5f58f
pedrojorge1997 <pedrojorgecunhapacheco@gmail.com>
2017-05-08 14:09:24 +0100

    Add files via upload

---
+++ discografia.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $(".discografia section a[href='']").click(function (e) {
+        e.preventDefault();
+    });
+
+    $(".discografia section img[src='#']").attr("src", "https://2.bp.blogspot.com/-HvmZ5SKRFWw/WQyIoO6HYmI/AAAAAAAAAPI/gqGPlDSmbhsxTvfUk96DEDvlg0cNX6_pACPcB/s1600/desconhecida.jpg");
+});
