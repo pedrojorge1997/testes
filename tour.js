42e69f0ab4608d7400799c3073e040bcc4e13e7f
pedrojorge1997 <pedrojorgecunhapacheco@gmail.com>
2017-05-08 13:46:17 +0100

    Add files via upload

---
+++ tour.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    var total = $("div.datas ul li").length;
+    var dataAtual = new Date().setHours(0, 0, 0, 0);
+    var elemento, data, i;
+
+    for (elemento = $("div.datas ul li").first(), i = 0; i < total; i++, elemento = elemento.next()) {
+        data = new Date(elemento.text().substring(0, 10));
+
+        if (data < dataAtual) {
+            elemento.addClass("passada");
+        }
+    }
+
+    $("div.datas ul li.passada").hover(
+        function () {
+            $(this).removeClass("passada");
+        },
+        function () {
+            $(this).addClass("passada");
+        });
+});
