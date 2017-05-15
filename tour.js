46fa25ce9a4581797ebec54d7c42819982ef1f8b
pedrojorge1997 <pedrojorgecunhapacheco@gmail.com>
2017-05-15 19:45:10 +0100

    Update tour.js

--- tour.js
+++ tour.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
     var total = $("div.datas ul li").length;
     var dataAtual = new Date().setHours(0, 0, 0, 0);
     var elemento, data, i;
-    
-    $("div.datas p.total").text("Total de concertos: " + total);
 
     for (elemento = $("div.datas ul li").first(), i = 0; i < total; i++, elemento = elemento.next()) {
         data = new Date(elemento.text().substring(0, 10));
